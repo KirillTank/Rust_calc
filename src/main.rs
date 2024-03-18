4aff63dce1e774feebb73df14aaab0323e87ebd5
KirillTank <kirillklemantovih0309@gmail.com>
2024-03-18 13:13:30 +0600

    rust calculator

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,51 @@
-use std::{error::Error, fmt::Display};
-
-
-#[derive(Debug)]
-enum CustomError{
-    error1(String),
-    error2(i32),
-    error3(i32, String)
-}
-impl Display for CustomError{
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            Self::error1(msg) => write!(f, "{} 1 =)", msg),
-            Self::error2(code) => write!(f, "cusstom error {} =)", code),
-            Self::error3(code, msg) => write!(f, "{} {} =)", msg, code),
-        }
-    }
-}
-impl Error for CustomError {}
-
-fn do_somethink() -> Result<(), CustomError>{
-    Err(CustomError::error3( 1, "error my love❤ ".to_owned() ))
+struct Calc{
+    num_1: i32,
+    num_2: i32,
+    znak: String,
 }
 
 fn main(){
-    match do_somethink() {
-        Ok(_) => {},
-        Err(err) => { println!("{err}")},
+    calc()
+}
+
+fn calc(){
+    let mut finnal = 0;
+    let mut a = Calc {
+        num_1: 0,
+        num_2: 0,
+        znak: "dada".to_string()
+    };
+
+    println!("число 1");
+    a.num_1 = read_console().parse::<i32>().unwrap();
+
+    println!("число 2");
+    a.num_2 = read_console().parse::<i32>().unwrap();
+
+    println!("Знак ( +, -, /, * )");
+    a.znak = read_console();
+
+    if a.znak == "-".to_string() {
+        finnal = a.num_1 - a.num_2;
+        println!("{}", finnal);
+    } else if a.znak == "+".to_string() {
+        finnal = a.num_1 + a.num_2;
+        println!("{}", finnal);
+    } else if a.znak == "/".to_string()  {
+        finnal = a.num_1 / a.num_2;
+        println!("{}", finnal);
+    } else if a.znak == "*".to_string()  {
+        finnal = a.num_1 * a.num_2;
+        println!("{}", finnal);
+    } else {
+        println!("нет такого знака пошол на");
     }
+}
+
+fn read_console() -> String{
+    let mut a = "".to_string();
+
+    std::io::stdin().read_line(&mut a).expect("error читать");
+
+    a.trim().to_string()
 }
